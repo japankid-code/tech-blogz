616348ddbc6e83275a6fe12a5460987ec40f9143
japankid-code <japankid.jake@gmail.com>
2021-05-24 20:25:11 -0500

    add model associations, wrap up #2

--- models/index.js
+++ models/index.js
@@ -1,3 +1,29 @@
 const Post = require('./Post');
 const User = require('./User');
 const Comment = require('./Comment');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+})
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+})
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+  foreignKey: 'user_id'
+})
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+})
+
+Post.hasMany(Comment, {
+  foreignKey: 'user_id'
+})
+
+module.exports = { User, Post, Comment };
