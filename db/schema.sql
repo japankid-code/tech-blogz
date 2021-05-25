18a945c2e62ab9b5f84a19da7dfb88c48d2f2345
japankid-code <japankid.jake@gmail.com>
2021-05-24 20:13:30 -0500

    add model definitions using sequelize, close #2

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS tech_blogz;
+
+CREATE DATABASE tech_blogz;
---
+++ models/index.js
@@ -0,0 +1,3 @@
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
