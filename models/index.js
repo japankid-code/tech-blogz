a7cb4d64cf880bae2911e0b127eebd0e7eac3396
japankid-code <japankid.jake@gmail.com>
2021-05-27 17:21:32 -0500

    semantic rearrangements

--- models/index.js
+++ models/index.js
@@ -14,12 +14,12 @@ Comment.belongsTo(User, {
   foreignKey: 'user_id'
 })
 
-Comment.belongsTo(Post, {
-  foreignKey: 'post_id'
-})
-
 User.hasMany(Comment, {
   foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
 })
 
 Post.hasMany(Comment, {
